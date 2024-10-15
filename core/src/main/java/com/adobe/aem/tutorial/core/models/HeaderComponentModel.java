bb01011e80a2de8d7be0678a9effb7069bf5063a
khanaaqib <35225284+khanaaqib@users.noreply.github.com>
2024-10-15 08:08:38 +0530

    Fetched Single field value via Sling Model

---
+++ core/src/main/java/com/adobe/aem/tutorial/core/models/HeaderComponentModel.java
@@ -0,0 +1,75 @@
+package com.adobe.aem.tutorial.core.models;
+
+
+import org.apache.sling.api.resource.Resource;
+import org.apache.sling.models.annotations.DefaultInjectionStrategy;
+import org.apache.sling.models.annotations.Model;
+
+import javax.inject.Inject;
+
+@Model(adaptables = Resource.class,defaultInjectionStrategy = DefaultInjectionStrategy.OPTIONAL)
+public class HeaderComponentModel {
+
+    @Inject
+    private String homePage;
+
+    @Inject
+    private String homePageUrl;
+
+    @Inject
+    private String aboutPage;
+
+    @Inject
+    private String signupUrl;
+
+    @Inject
+    private String signupPage;
+
+    @Inject
+    private String loginPagePath;
+
+    @Inject
+    private String logintitle;
+
+    @Inject
+    private String myaccountPagePath;
+
+    @Inject
+    private String myaccountTitle;
+
+    public String getHomePage() {
+        return homePage;
+    }
+
+    public String getHomePageUrl() {
+        return homePageUrl;
+    }
+
+    public String getAboutPage() {
+        return aboutPage;
+    }
+
+    public String getSignupUrl() {
+        return signupUrl;
+    }
+
+    public String getSignupPage() {
+        return signupPage;
+    }
+
+    public String getLoginPagePath() {
+        return loginPagePath;
+    }
+
+    public String getLogintitle() {
+        return logintitle;
+    }
+
+    public String getMyaccountPagePath() {
+        return myaccountPagePath;
+    }
+
+    public String getMyaccountTitle() {
+        return myaccountTitle;
+    }
+}
